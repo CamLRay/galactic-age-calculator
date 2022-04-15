785f78fd6da7968ce0cce5d1ac6368944bf7f4b8
Sploob <crayiphone@gmail.com>
2022-04-15 09:06:21 -0700

    add test for currentAge method

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -4,7 +4,12 @@ import { GalacticAge } from "../src/js/mercury";
 describe("Galactic Age", () => {
   test("it should create an object with an empty planet property for earth", () => {
     let galacticAge = new GalacticAge();
-    expect(galacticAge.earth).toEqual("")
+    expect(galacticAge.earth).toEqual("");
   });
 
+  test("it should add a users age to the earth property", () => {
+    let galacticAge = new GalacticAge();
+    galacticAge.currentAge(29);
+    expect(galacticAge.earth).toEqual(29);
+  });
 });
