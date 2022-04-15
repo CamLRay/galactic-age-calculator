d00cd8135a508a693a2c823a36c9aa36fe66118c
Sploob <crayiphone@gmail.com>
2022-04-15 11:06:24 -0700

    pass second test for lifeLeft method

--- src/js/mercury.js
+++ src/js/mercury.js
@@ -16,5 +16,12 @@ export class GalacticAge {
 
   lifeLeft(expectancy) {
     this.earthLeft = parseFloat((expectancy - this.earth).toFixed(4).slice(0,-2));
+    this.mercuryLeft = parseFloat((this.earthLeft/.24).toFixed(4).slice(0,-2));
+    this.venusLeft = parseFloat((this.earthLeft/.62).toFixed(4).slice(0,-2));
+    this.marsLeft = parseFloat((this.earthLeft/1.88).toFixed(4).slice(0,-2));
+    this.jupiterLeft = parseFloat((this.earthLeft/11.86).toFixed(4).slice(0,-2));
+    this.saturnLeft = parseFloat((this.earthLeft/29.457).toFixed(4).slice(0,-2));
+    this.uranusLeft = parseFloat((this.earthLeft/84.011).toFixed(4).slice(0,-2));
+    this.neptuneLeft = parseFloat((this.earthLeft/164.78).toFixed(4).slice(0,-2));
   }
 }
